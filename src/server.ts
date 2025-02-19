18d4bf06dfe2dd0922ad190be5e9ecacb8bfed77
Kanika Kalia <kanika@bhangar.net>
2025-02-19 19:44:31 +0545

    make database connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import app from './app';
 import config from 'config'
 import logger from './config/logger';
+import { initDB } from './config/db';
 
-const startServer = () => {
+const startServer = async() => {
   const PORT=config.get('server.port') || 5500
   try {
+    await initDB()
+    logger.info('Database connected successfully')
     app.listen(PORT, () => {
       logger.info(`Server Running at port ${PORT}`);
     });
